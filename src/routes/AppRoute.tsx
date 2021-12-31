de5e1f9284cca5d7c24235fdb545accbaa76cb7f
Daniel Marques <danielmarques7@hotmail.com>
2021-12-31 20:52:05 -0300

    Create react app with the typescript template

---
+++ src/routes/AppRoute.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { SignIn } from '../pages/SignIn/SignIn';
+
+export const AppRoute = () => (
+  <Routes>
+    {/* <Route path="/" element={<Home />} /> */}
+    <Route path="/signin" element={<SignIn />} />
+  </Routes>
+);
