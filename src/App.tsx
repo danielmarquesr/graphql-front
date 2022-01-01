9e4a290892d15685f83f030767aa87fdd7d0b47f
Daniel Marques <danielmarques7@hotmail.com>
2022-01-01 20:24:11 -0300

    Create Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './assets/css/App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import { AppRoute } from './routes/AppRoute';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => (
   <div className="App">
     <BrowserRouter>
+      <ScrollToTop />
       <AppRoute />
     </BrowserRouter>
   </div>
--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { SignIn } from 'src/pages/SignIn/SignIn';
+import { Home, SignIn } from 'src/pages';
 
 export const AppRoute = () => (
   <Routes>
-    {/* <Route path="/" element={<Home />} /> */}
+    <Route path="/" element={<Home />} />
     <Route path="/signin" element={<SignIn />} />
   </Routes>
 );
