a9a6908d119a05c413c1e67497198d66a598383f
Daniel Marques <danielmarques7@hotmail.com>
2022-01-15 20:30:02 -0300

    Add yup and formik validations to SignIn page

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useMutation } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 import { PayloadError } from 'relay-runtime';
+import { Link, useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { SignInMutation } from './__generated__/SignInMutation.graphql';
 
 const mutation = graphql`
@@ -12,34 +15,50 @@ const mutation = graphql`
   }
 `;
 
+interface Values {
+  email: string;
+  password: string;
+}
+
+const validationSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).max(50).required(),
+});
+
 export const SignIn = () => {
-  const [commit, isInFlight] = useMutation<SignInMutation>(mutation);
+  const [commit, loading] = useMutation<SignInMutation>(mutation);
+  const navigate = useNavigate();
 
   const [errors, setErrors] = useState(null as PayloadError[] | null);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
-    event.preventDefault();
+  const formik = useFormik<Values>({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit: () => {
+      const { email, password } = formik.values;
+      commit({
+        variables: { input: { email, password } },
+        onCompleted: (data, error) => {
+          const token = data.SignIn?.token;
 
-    setErrors(null);
+          if (!token) {
+            setErrors(error);
+            return;
+          }
 
-    commit({
-      variables: { input: { email, password } },
-      onCompleted: (data, error) => {
-        const token = data.SignIn?.token;
-        if (!token) {
-          setErrors(error);
-          return;
-        }
-        localStorage.setItem('jwt-token', token);
-      },
-    });
-  };
+          localStorage.setItem('token', token);
+          navigate('/');
+        },
+      });
+    },
+  });
 
   return (
     <>
-      <h1>SignIn</h1>
+      <h1>Sign In</h1>
 
       <div>
         {errors?.map(({ message }) => (
@@ -47,13 +66,14 @@ export const SignIn = () => {
         ))}
       </div>
 
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <input
           type="email"
           placeholder="email"
           name="email"
           id="email"
-          onChange={({ target }) => setEmail(target.value)}
+          onChange={formik.handleChange}
+          value={formik.values.email}
         />
         <br />
         <br />
@@ -62,13 +82,19 @@ export const SignIn = () => {
           placeholder="password"
           name="password"
           id="password"
-          onChange={({ target }) => setPassword(target.value)}
+          onChange={formik.handleChange}
+          value={formik.values.password}
         />
         <br />
         <br />
-        <button type="submit" disabled={isInFlight} onClick={handleSubmit}>
-          {isInFlight ? 'Loading...' : 'Sign In'}
+        <button
+          type="submit"
+          disabled={!formik.isValid || !formik.dirty || loading}
+        >
+          {loading ? 'Loading' : 'Submit'}
         </button>
+
+        <Link to="/signup">Sign Up</Link>
       </form>
     </>
   );
--- src/pages/SignIn/__generated__/SignInMutation.graphql.ts
+++ src/pages/SignIn/__generated__/SignInMutation.graphql.ts
@@ -28,68 +28,68 @@ export type SignInMutation = {
   response: SignInMutation$data;
 };
 
-const node: ConcreteRequest = (function () {
-  var v0 = [
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "input"
+  }
+],
+v1 = [
+  {
+    "alias": null,
+    "args": [
       {
-        defaultValue: null,
-        kind: 'LocalArgument',
-        name: 'input',
-      },
+        "kind": "Variable",
+        "name": "input",
+        "variableName": "input"
+      }
     ],
-    v1 = [
+    "concreteType": "Token",
+    "kind": "LinkedField",
+    "name": "SignIn",
+    "plural": false,
+    "selections": [
       {
-        alias: null,
-        args: [
-          {
-            kind: 'Variable',
-            name: 'input',
-            variableName: 'input',
-          },
-        ],
-        concreteType: 'Token',
-        kind: 'LinkedField',
-        name: 'SignIn',
-        plural: false,
-        selections: [
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'token',
-            storageKey: null,
-          },
-        ],
-        storageKey: null,
-      },
-    ];
-  return {
-    fragment: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Fragment',
-      metadata: null,
-      name: 'SignInMutation',
-      selections: v1 /*: any*/,
-      type: 'Mutation',
-      abstractKey: null,
-    },
-    kind: 'Request',
-    operation: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Operation',
-      name: 'SignInMutation',
-      selections: v1 /*: any*/,
-    },
-    params: {
-      cacheID: '3ee53fb672851ee4dac2df4fe5454b45',
-      id: null,
-      metadata: {},
-      name: 'SignInMutation',
-      operationKind: 'mutation',
-      text: 'mutation SignInMutation(\n  $input: AuthInput!\n) {\n  SignIn(input: $input) {\n    token\n  }\n}\n',
-    },
-  };
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "token",
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "SignInMutation",
+    "selections": (v1/*: any*/),
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "SignInMutation",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "3ee53fb672851ee4dac2df4fe5454b45",
+    "id": null,
+    "metadata": {},
+    "name": "SignInMutation",
+    "operationKind": "mutation",
+    "text": "mutation SignInMutation(\n  $input: AuthInput!\n) {\n  SignIn(input: $input) {\n    token\n  }\n}\n"
+  }
+};
 })();
 
-(node as any).hash = 'fa7d9c86ebfa331efa25100f5aef5f45';
+(node as any).hash = "fa7d9c86ebfa331efa25100f5aef5f45";
 
 export default node;
