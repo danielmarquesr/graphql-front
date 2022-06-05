93be55d9e035038c09296184b9b2b78ec9ed68e7
Daniel Marques <danielmarques7@hotmail.com>
2022-06-05 11:53:00 -0300

    Add Header component in Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,7 @@
-import React, { Suspense } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
 import { graphql } from 'babel-plugin-relay/macro';
 import { useLazyLoadQuery } from 'react-relay';
-import { ErrorBoundary } from 'src/components';
+import { Header } from 'src/components';
 import { HomeCurrentUserQuery } from './__generated__/HomeCurrentUserQuery.graphql';
 
 const CurrentUserQuery = graphql`
@@ -16,61 +15,20 @@ const CurrentUserQuery = graphql`
   }
 `;
 
-const Header = () => {
-  const navigate = useNavigate();
-
+export const Home = () => {
   const data = useLazyLoadQuery<HomeCurrentUserQuery>(
     CurrentUserQuery,
     {},
     { fetchPolicy: 'store-or-network' }
   );
 
-  const logout = () => {
-    localStorage.removeItem('token');
-    navigate('/');
-  };
-
-  return (
-    <div>
-      {data.CurrentUser ? (
-        <>
-          <div>
-            ID: {data.CurrentUser.id}
-            <br />
-            Email: {data.CurrentUser.email}
-            <br />
-            First name: {data.CurrentUser.firstName}
-            <br />
-            Last name: {data.CurrentUser.lastName}
-          </div>
-          <br />
-          <div>
-            <button type="button" onClick={logout}>
-              Logout
-            </button>
-          </div>
-        </>
-      ) : (
-        <Link to="/signin">Sign In</Link>
-      )}
-    </div>
-  );
-};
-
-export const Home = () => {
   return (
     <>
       <h1>Home</h1>
 
-      <ErrorBoundary
-        fallback={({ error }: { error: Error }) =>
-          `Error: ${JSON.stringify(error)}`
-        }
-      >
-        <Suspense fallback="Loading...">
-          <Header />
-        </Suspense>
-      </ErrorBoundary>
+      <p>Data: {JSON.stringify(data)}</p>
+
+      <Header data={data} />
     </>
   );
 };
