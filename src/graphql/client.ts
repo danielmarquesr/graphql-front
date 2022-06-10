3e13298bea21e6ed3c49c0b7d2279393a443693f
Daniel Marques <danielmarques7@hotmail.com>
2022-06-10 00:00:36 -0300

    Create GraphQLClient singleton class

---
+++ src/graphql/client.ts
@@ -0,0 +1,21 @@
+import { GraphQLClient } from 'graphql-request';
+
+const { REACT_APP_BACKEND_DOMAIN } = process.env;
+export const GRAPHQL_ENDPOINT = `${REACT_APP_BACKEND_DOMAIN}/graphql`;
+
+export class GraphQLClient2 {
+  private static instance: GraphQLClient;
+
+  constructor() {
+    if (!GraphQLClient2.instance) {
+      GraphQLClient2.instance = new GraphQLClient(GRAPHQL_ENDPOINT);
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getInstance() {
+    return GraphQLClient2.instance;
+  }
+}
+
+export const client = new GraphQLClient2().getInstance();
